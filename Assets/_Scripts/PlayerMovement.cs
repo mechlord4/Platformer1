661bb23e8afdf75aacc88c209c6a8d699e1bddf8
mechlord4 <Shaynewt1@gmail.com>
2020-01-24 14:38:11 -0500

    PlatformerCode

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float xmov,speed, jumpheight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
